dc83b2de378c03faf65b7cb8323a2287cdb36fa3
hossi97 <asd8500@naver.com>
2024-10-16 23:30:11 +0900

    스프링 컨테이너와 Bean 구성 정보

--- src/main/java/com/hossi/spring6/ObjectFactory.java
+++ src/main/java/com/hossi/spring6/ObjectFactory.java
@@ -1,11 +1,22 @@
 package com.hossi.spring6;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * BeanFactory 에 제공할 Bean 클래스 & 의존관계에 대한 구성 정보
+ * 구성 정보를 바탕으로 스프링 컨테이녀(= BeanFactory)가 구현체를 생성 및 의존관계를 주입하고, 구현체를 제공
+ * (기본적으로 구현체를 Singleton 으로 유지)
+ */
+@Configuration
 public class ObjectFactory {
+  @Bean
   public PaymentService paymentService() {
     return new PaymentService(exchangeRateProvider());
   }
 
-  private ExchangeProvider exchangeRateProvider() {
+  @Bean
+  public ExchangeProvider exchangeRateProvider() {
     return new WebApiExchangeRateProvider();
   }
 }
