3cfd59dcb2ef84feb755ed8e3476680b52f13c19
hossi97 <asd8500@naver.com>
2024-10-21 09:52:03 +0900

    JPA 트랜잭션과 예외

--- src/main/java/com/hossi/spring6/DataClient.java
+++ src/main/java/com/hossi/spring6/DataClient.java
@@ -9,15 +9,34 @@ import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class DataClient {
 
   public static void main(String[] args) {
     BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
     OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
-    Order order = new Order("100", BigDecimal.TEN);
-    orderRepository.save(order);
-    System.out.println(order);
+    JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
+
+    try {
+      new TransactionTemplate(transactionManager).execute(status -> {
+        Order order1 = new Order("100", BigDecimal.TEN);
+        Order order2 = new Order("100", BigDecimal.ONE);
+        orderRepository.save(order1);
+        orderRepository.save(order2);
+        return null;
+      });
+    } catch (DataIntegrityViolationException e) {
+      // DataAccessException 의 서브 클래스로 Spring Repository or DAO 에서 발생하는 예외는 DataAccessException 로 래핑되어 던져짐
+      System.out.println("중복된 주문번호 저장");
+      throw new RuntimeException(e);
+    }
+
   }
 
 }
--- src/main/java/com/hossi/spring6/data/OrderRepository.java
+++ src/main/java/com/hossi/spring6/data/OrderRepository.java
@@ -4,32 +4,18 @@ import com.hossi.spring6.order.Order;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import org.hibernate.HibernateException;
 import org.springframework.dao.DataAccessException;
 
 public class OrderRepository {
-  private final EntityManagerFactory emf;
-
-  public OrderRepository(EntityManagerFactory emf) {
-    this.emf = emf;
-  }
+  @PersistenceContext
+  private EntityManager em;
 
   public void save(Order order) {
-    EntityManager em = emf.createEntityManager();
-    EntityTransaction transaction = em.getTransaction();
-    transaction.begin();
-
-    try (em) {
-      em.persist(order);
-      transaction.commit();
-    } catch (DataAccessException e) {
-      if (transaction.isActive()) {
-        transaction.rollback();
-      }
-      throw new RuntimeException(e);
-    }
+    em.persist(order);
   }
 
 }
